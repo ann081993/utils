5d58eb544268dfdbe2b0db1ad618bb72e32f37a6
ann081993 <ann081993@snu.ac.kr>
2022-04-20 10:11:10 +0900

    Update utils.R

--- utils.R
+++ utils.R
@@ -39,7 +39,7 @@ htable <- function(x, n = 6) {
 pbar <- function() {
         # library(progress)
         l <- 10 # length of for loop
-        pb <- progress_bar$new(format = "Progress: [:bar] :percent, Elapsed tile :elapsedfull",
+        pb <- progress_bar$new(format = "Progress: [:bar] :percent, Elapsed time :elapsedfull",
                                total = l, width = 80, clear = F, force = T)
         # add tick in for loop
         for(n in 1:l) {
