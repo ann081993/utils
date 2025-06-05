894f52c192aa5a7cb4aeac771a1786b7f59154c2
Seungchan An <ann081993@snu.ac.kr>
2025-06-05 11:22:57 +0900

    Update utils.R

--- utils.R
+++ utils.R
@@ -170,3 +170,9 @@ theme_dot <- function() {
                         axis.title.x = element_blank(),
                         axis.title.y = element_blank())
 }
+
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
+
